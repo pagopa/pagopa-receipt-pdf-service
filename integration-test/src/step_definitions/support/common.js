3c0511772ca3289cd477f6f446bd6c22a212f4ac
giomella <gioele.mella.st@everis.nttdata.com>
2023-07-27 11:05:16 +0200

    [PRDP-22] removed useless log

--- integration-test/src/step_definitions/support/common.js
+++ integration-test/src/step_definitions/support/common.js
@@ -45,7 +45,6 @@ function httpGET(url, fiscalCode) {
             }
             console.log("ERROR MESSAGE ");
             console.log(error.message);
-            console.log(error.config);
             console.log("---------------------");
 			return error.response;
 		});
