a94acd94d802e65ecd9e4ab5fcad595b72326c05
svariant <samuele.varianti@nttdata.com>
2023-08-02 11:06:04 +0200

    [PRDP-24] Configured performance tests

--- performance-test/README.md
+++ performance-test/README.md
@@ -4,10 +4,11 @@ This is a set of [k6](https://k6.io) tests.
 
 To invoke k6 tests use `run_performance_test.sh` script.
 
+
 ## How to run 🚀
 
 Use this command to launch the tests:
 
 ``` shell
-sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-filename> <db-name> <subkey>
+sh run_performance_test.sh <local|dev|uat> <load|stress|spike|...> <script-filename> <DB-name> <blob_storage_conn_string> <cosmos_receipt_key>
 ```
--- performance-test/run_performance_test.sh
+++ performance-test/run_performance_test.sh
@@ -1,42 +1,35 @@
-# sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <subkey>
+# sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_conn_string> <cosmos_receipt_key>
 
 ENVIRONMENT=$1
 TYPE=$2
 SCRIPT=$3
 DB_NAME=$4
-API_SUBSCRIPTION_KEY=$5
+BLOB_STORAGE_CONN_STRING=$5
+COSMOS_RECEIPT_KEY=$6
 
 if [ -z "$ENVIRONMENT" ]
 then
-  echo "No env specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <subkey>"
+  echo "No env specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_conn_string> <cosmos_receipt_key>"
   exit 1
 fi
 
 if [ -z "$TYPE" ]
 then
-  echo "No test type specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <subkey>"
+  echo "No test type specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_conn_string> <cosmos_receipt_key>"
   exit 1
 fi
 if [ -z "$SCRIPT" ]
 then
-  echo "No script name specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <subkey>"
+  echo "No script name specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_conn_string> <cosmos_receipt_key>"
   exit 1
-fi
-
-if [ -z "$DB_NAME" ]
-then
-  DB_NAME="k6"
-  echo "No DB name specified: 'k6' is used."
 fi
 
 export env=${ENVIRONMENT}
 export type=${TYPE}
 export script=${SCRIPT}
 export db_name=${DB_NAME}
-export sub_key=${API_SUBSCRIPTION_KEY}
-
-docker rm nginx
-docker rm k6
+export blob_storage_conn_string=${BLOB_STORAGE_CONN_STRING}
+export cosmos_receipt_key=${COSMOS_RECEIPT_KEY}
 
 stack_name=$(cd .. && basename "$PWD")
 docker compose -p "${stack_name}-k6" up -d --remove-orphans --force-recreate --build
