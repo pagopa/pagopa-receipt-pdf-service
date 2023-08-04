1feb5c0491f611c84d4175ab055b615b0a5bee69
svariant <samuele.varianti@nttdata.com>
2023-08-04 16:02:07 +0200

    [PRDP-24] Receipt Service client

--- performance-test/src/modules/receipt_service_client.js
+++ performance-test/src/modules/receipt_service_client.js
@@ -2,34 +2,13 @@ import http from 'k6/http';
 
 const subKey = `${__ENV.SUBSCRIPTION_KEY}`;
 
-const varsArray = new SharedArray('vars', function () {
-    return JSON.parse(open(`./${__ENV.VARS}`)).environment;
-  });
-const vars = varsArray[0];
-const receiptServiceURIBasePath = `${vars.receiptServiceURIBasePath}`;
-const receiptServiceGetAttachmentPath = `${vars.receiptServiceGetAttachmentPath}`;
-const receiptServiceGetAttachmentDetailsPath = `${vars.receiptServiceGetAttachmentDetailsPath}`;
-
-export function getAttachment(receiptId, requestFiscalCode, attachmentUrl) {
-    const form = {
-        thirdPartyId:receiptId, requestFiscalCode, attachmentUrl
-      };
+export function postToService(url, request) {
+    const form = request;
 
       let headers = { 
         'Ocp-Apim-Subscription-Key': subKey
     };
 
-    return http.post(receiptServiceURIBasePath+receiptServiceGetAttachmentPath, form, {headers});
+    return http.post(url, form, {headers});
 }
 
-export function getAttachmentDetails(receiptId, requestFiscalCode) {
-    const form = {
-        thirdPartyId:receiptId, requestFiscalCode
-      };
-
-      let headers = { 
-        'Ocp-Apim-Subscription-Key': subKey
-    };
-
-    return http.post(receiptServiceURIBasePath+receiptServiceGetAttachmentDetailsPath, form, {headers});
-}
