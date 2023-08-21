802ce9de4a8ba3a99e996696575201757d4d25d6
svariant <samuele.varianti@nttdata.com>
2023-08-21 17:47:08 +0200

    [PRDP-24] Changed blob storage client methods from azure library to k6 http

--- performance-test/src/modules/receipt_blob_storage_client.js
+++ performance-test/src/modules/receipt_blob_storage_client.js
@@ -1,42 +1,50 @@
-import { BlobServiceClient } from "@azure/storage-blob";
+import http from 'k6/http';
+import crypto from 'k6/crypto';
+import encoding from 'k6/encoding';
 
-const getContainerClient = (blobConnString, blobContainerId) => {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(
-        blobConnString
-      );
-      const containerClient = blobServiceClient.getContainerClient(
-        blobContainerId
-      );
+const blobMsVersion = "2022-11-02";
 
-      return containerClient;
+function getStorageAPIHeaders(method, accountName, key, length) {
+  let strTime = (new Date()).toUTCString();
+  let strToSign = `${method}\n\n\n\n\n\n\n\n\n\n\n\nx-ms-date:${strTime}\nx-ms-version:${blobMsVersion}\n/${accountName}/\ncomp:block`;
+  let secret = encoding.b64decode(key);
+  let hmacSha256 = crypto.createHMAC("sha256", secret);
+  hmacSha256.update(strToSign);
+  let hashInBase64 = hmacSha256.digest("base64");
+
+  let auth = `SharedKey ${accountName}:${hashInBase64}`;
+
+  return {
+    'Authorization': auth,
+    'x-ms-version': blobMsVersion,
+    'x-ms-date': strTime,
+    "Content-Length": length
+  };
 }
 
-export const uploadDocumentToAzure = async (blobConnString, blobContainerId, pdfName, pdfFile) => {
-    const containerClient = getContainerClient(blobConnString, blobContainerId);
+export async function uploadDocumentToAzure(accountName, storageKey, containerId, blobId, document) {
+  console.log("ASd", document.length);
+  const headers = getStorageAPIHeaders("PUT", accountName, storageKey, document.length );
 
-    const data = Buffer.from(pdfFile, "base64");
-    const blockBlobClient = containerClient.getBlockBlobClient(pdfName);
-    const response = await blockBlobClient.uploadData(data, {
-      blobHTTPHeaders: {
-        blobContentType: "application/pdf",
-      },
-    });
-    if (response._response.status !== 201) {
-      throw new Error(
-        `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`
-      );
-    }
+  const body = document;
 
-    return response;
-  };
+  
 
-export const deleteDocumentFromAzure = async (blobConnString, blobContainerId, pdfName) => {
-    const containerClient = getContainerClient(blobConnString, blobContainerId);
+  let resp = http.put(`https://${accountName}.blob.core.windows.net/${containerId}/${blobId}.pdf`, body, { headers });
 
-    const response = await containerClient.deleteBlob(pdfName);
-    if (response._response.status !== 202) {
-      throw new Error(`Error deleting ${pdfName}`);
-    }
+  console.log("SAMU", resp);
 
-    return response;
-  };
+  return resp;
+};
+
+export async function deleteDocumentFromAzure(accountName, storageKey, containerId, blobId) {
+
+  const headers = getStorageAPIHeaders("DELETE", accountName, storageKey);
+
+  let response = http.del(`https://${accountName}.blob.core.windows.net/${containerId}/${blobId}.pdf`, { headers });
+  if (response._response.status !== 202) {
+    throw new Error(`Error deleting ${blobId}`);
+  }
+
+  return response;
+};
