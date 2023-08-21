aa721b69e2ee5343c3f0d083ab62709e0811cb51
svariant <samuele.varianti@nttdata.com>
2023-08-21 17:46:40 +0200

    [PRDP-24] Fixed cosmos client imports

--- performance-test/src/modules/receipt_cosmos_client.js
+++ performance-test/src/modules/receipt_cosmos_client.js
@@ -1,3 +1,37 @@
+import http from 'k6/http';
+import crypto from 'k6/crypto';
+import encoding from 'k6/encoding';
+
+const authorizationType      = "master"
+const authorizationVersion   = "1.0";
+const cosmosDBApiVersion     = "2018-12-31";
+
+function getCosmosDBAuthorizationToken(verb, autorizationType, autorizationVersion, authorizationSignature, resourceType, resourceLink, dateUtc) {
+    // Decode authorization signature
+    let key = encoding.b64decode(authorizationSignature);
+    let text = (verb || "").toLowerCase() + "\n" +
+        (resourceType || "").toLowerCase() + "\n" +
+        (resourceLink || "") + "\n" +
+        dateUtc.toLowerCase() + "\n\n";
+    let hmacSha256 = crypto.createHMAC("sha256", key);
+    hmacSha256.update(text);
+    // Build autorization token, encode it and return
+    return encodeURIComponent("type=" + autorizationType + "&ver=" + autorizationVersion + "&sig=" + hmacSha256.digest("base64"));
+}
+
+function getCosmosDBAPIHeaders(authorizationToken, date, partitionKeyArray, contentType, isQuery){
+
+    return {'Accept': 'application/json',
+        'Content-Type': contentType,
+        'Authorization': authorizationToken,
+        'x-ms-version': cosmosDBApiVersion,
+        'x-ms-date': date,
+        'x-ms-documentdb-isquery': isQuery ? isQuery : "false",
+        'x-ms-query-enable-crosspartition': 'true',
+        'x-ms-documentdb-partitionkey': partitionKeyArray
+    };
+}
+
 export function createDocument(cosmosDbURI, databaseId, containerId, authorizationSignature, document, pk) {
 	let path = `dbs/${databaseId}/colls/${containerId}/docs`;
 	let resourceLink = `dbs/${databaseId}/colls/${containerId}`;
