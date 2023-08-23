ce7baa2d537ece75b290691f77fbb9adc9495b21
svariant <samuele.varianti@nttdata.com>
2023-08-23 18:28:52 +0200

    [PRDP-24] Updated performance test scripts - readme - docker yaml

--- performance-test/run_performance_test.sh
+++ performance-test/run_performance_test.sh
@@ -1,26 +1,25 @@
-# sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_key> <cosmos_receipt_key>
+# sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <apim_subscription_key>
 
 ENVIRONMENT=$1
 TYPE=$2
 SCRIPT=$3
 DB_NAME=$4
-BLOB_STORAGE_KEY=$5
-COSMOS_RECEIPT_KEY=$6
+SUBSCRIPTION_KEY=$5
 
 if [ -z "$ENVIRONMENT" ]
 then
-  echo "No env specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_key> <cosmos_receipt_key>"
+  echo "No env specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <apim_subscription_key>"
   exit 1
 fi
 
 if [ -z "$TYPE" ]
 then
-  echo "No test type specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_key> <cosmos_receipt_key>"
+  echo "No test type specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <apim_subscription_key>"
   exit 1
 fi
 if [ -z "$SCRIPT" ]
 then
-  echo "No script name specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <blob_storage_key> <cosmos_receipt_key>"
+  echo "No script name specified: sh run_performance_test.sh <local|dev|uat|prod> <load|stress|spike|soak|...> <script-name> <db-name> <apim_subscription_key>"
   exit 1
 fi
 
@@ -28,8 +27,7 @@ export env=${ENVIRONMENT}
 export type=${TYPE}
 export script=${SCRIPT}
 export db_name=${DB_NAME}
-export blob_storage_key=${BLOB_STORAGE_KEY}
-export cosmos_receipt_key=${COSMOS_RECEIPT_KEY}
+export subscription_key=${SUBSCRIPTION_KEY}
 
 stack_name=$(cd .. && basename "$PWD")
 docker compose -p "${stack_name}-k6" up -d --remove-orphans --force-recreate --build
