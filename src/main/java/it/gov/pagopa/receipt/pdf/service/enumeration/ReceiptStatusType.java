6523fbc9af92be554a6dfe06c6f4ad909e36680c
svariant <samuele.varianti@nttdata.com>
2023-11-30 17:03:12 +0100

    [PRDP-258] Added new Receipt status TO_REVIEW

--- src/main/java/it/gov/pagopa/receipt/pdf/service/enumeration/ReceiptStatusType.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/service/enumeration/ReceiptStatusType.java
@@ -4,5 +4,5 @@ package it.gov.pagopa.receipt.pdf.service.enumeration;
  * Enumeration of the receipt status
  */
 public enum ReceiptStatusType {
-    NOT_QUEUE_SENT, INSERTED, RETRY, GENERATED, SIGNED, FAILED, IO_NOTIFIED
+    NOT_QUEUE_SENT, INSERTED, RETRY, GENERATED, SIGNED, FAILED, IO_NOTIFIED, TO_REVIEW
 }
