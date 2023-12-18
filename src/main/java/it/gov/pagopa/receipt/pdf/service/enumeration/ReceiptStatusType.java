3148882d9033d037f2f6a601ca4ad010f3d8b8bb
giomella <gioele.mella@emeal.nttdata.com>
2023-12-18 16:42:01 +0100

    add missing receipt status

--- src/main/java/it/gov/pagopa/receipt/pdf/service/enumeration/ReceiptStatusType.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/service/enumeration/ReceiptStatusType.java
@@ -4,5 +4,5 @@ package it.gov.pagopa.receipt.pdf.service.enumeration;
  * Enumeration of the receipt status
  */
 public enum ReceiptStatusType {
-    NOT_QUEUE_SENT, INSERTED, RETRY, GENERATED, SIGNED, FAILED, IO_NOTIFIED, TO_REVIEW
+    NOT_QUEUE_SENT, INSERTED, RETRY, GENERATED, SIGNED, FAILED, IO_NOTIFIED, IO_ERROR_TO_NOTIFY, IO_NOTIFIER_RETRY, UNABLE_TO_SEND, NOT_TO_NOTIFY
 }
