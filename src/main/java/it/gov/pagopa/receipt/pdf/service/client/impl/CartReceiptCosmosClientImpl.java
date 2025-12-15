956243baa92ee532f660c8ccec464e26e2b70885
Matteo Raiola <matteo.raiola@nttdata.com>
2025-12-15 11:36:36 +0100

    fix: [PAGOPA-3416] code formatting due to axel-op googlejavaformat-action version change

--- src/main/java/it/gov/pagopa/receipt/pdf/service/client/impl/CartReceiptCosmosClientImpl.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/service/client/impl/CartReceiptCosmosClientImpl.java
@@ -15,36 +15,36 @@ import org.slf4j.LoggerFactory;
 
 import static it.gov.pagopa.receipt.pdf.service.utils.CommonUtils.sanitize;
 
-/**
- * Client for the CosmosDB database
- */
+/** Client for the CosmosDB database */
 @ApplicationScoped
 public class CartReceiptCosmosClientImpl implements CartReceiptCosmosClient {
 
-    private final Logger logger = LoggerFactory.getLogger(CartReceiptCosmosClientImpl.class);
+  private final Logger logger = LoggerFactory.getLogger(CartReceiptCosmosClientImpl.class);
 
-    @CartContainer
-    private final CosmosContainer containerCartReceipts;
+  @CartContainer private final CosmosContainer containerCartReceipts;
 
-    @Inject
-    public CartReceiptCosmosClientImpl(@CartContainer CosmosContainer containerCartReceipts) {
-        this.containerCartReceipts = containerCartReceipts;
+  @Inject
+  public CartReceiptCosmosClientImpl(@CartContainer CosmosContainer containerCartReceipts) {
+    this.containerCartReceipts = containerCartReceipts;
+  }
+
+  public CartForReceipt getCartForReceiptDocument(String cartId) throws CartNotFoundException {
+    // Build query
+    String query = String.format("SELECT * FROM c WHERE c.eventId = '%s'", cartId);
+
+    // Query the container
+    CosmosPagedIterable<CartForReceipt> queryResponse =
+        containerCartReceipts.queryItems(
+            query, new CosmosQueryRequestOptions(), CartForReceipt.class);
+
+    if (!queryResponse.iterator().hasNext()) {
+      String errMsg =
+          String.format(
+              "Cart with id %s not found in the defined container: %s",
+              sanitize(cartId), containerCartReceipts.getId());
+      logger.error(errMsg);
+      throw new CartNotFoundException(AppErrorCodeEnum.PDFS_801, errMsg, cartId);
     }
-
-
-    public CartForReceipt getCartForReceiptDocument(String cartId) throws CartNotFoundException {
-        //Build query
-        String query = String.format("SELECT * FROM c WHERE c.eventId = '%s'", cartId);
-
-        //Query the container
-        CosmosPagedIterable<CartForReceipt> queryResponse = containerCartReceipts
-                .queryItems(query, new CosmosQueryRequestOptions(), CartForReceipt.class);
-
-        if (!queryResponse.iterator().hasNext()) {
-            String errMsg = String.format("Cart with id %s not found in the defined container: %s", sanitize(cartId), containerCartReceipts.getId());
-            logger.error(errMsg);
-            throw new CartNotFoundException(AppErrorCodeEnum.PDFS_801, errMsg, cartId);
-        }
-        return queryResponse.iterator().next();
-    }
+    return queryResponse.iterator().next();
+  }
 }
