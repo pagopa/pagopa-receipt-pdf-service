a73506f12f57d48318cbe7b81f5963bba91e6b7c
svariant <samuele.varianti@nttdata.com>
2026-01-14 16:58:59 +0100

    feat: [PAGOPA-3454] Integration tests bizevent client

---
+++ integration-test/src/step_definitions/support/biz_events_datastore_client.js
@@ -0,0 +1,43 @@
+const { CosmosClient } = require("@azure/cosmos");
+const { createEvent, createEventWithIUVAndOrgCode } = require("./common");
+
+const cosmos_db_conn_string = process.env.BIZEVENTS_COSMOS_CONN_STRING;
+const databaseId = process.env.BIZ_EVENT_COSMOS_DB_NAME;  // es. db
+const containerId = process.env.BIZ_EVENT_COSMOS_DB_CONTAINER_NAME; // es. biz-events
+
+const client = new CosmosClient(cosmos_db_conn_string);
+const container = client.database(databaseId).container(containerId);
+
+async function createDocumentInBizEventsDatastore(id, status) {
+    let event = createEvent(id, status);
+    try {
+        return await container.items.create(event);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function createDocumentInBizEventsDatastoreWithIUVAndOrgCode(id, status, orgCode, iuv) {
+    let event = createEventWithIUVAndOrgCode(id, status, orgCode, iuv);
+    try {
+        return await container.items.create(event);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteDocumentFromBizEventsDatastore(id) {
+    try {
+        return await container.item(id, id).delete();
+    } catch (error) {
+        if (error.code !== 404) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = {
+    createDocumentInBizEventsDatastore, 
+    createDocumentInBizEventsDatastoreWithIUVAndOrgCode,
+    deleteDocumentFromBizEventsDatastore,
+}
