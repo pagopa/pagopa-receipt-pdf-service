c212d1036eaeca86bef4530ec959ca720bb11a6a
svariant <samuele.varianti@nttdata.com>
2026-01-14 17:01:38 +0100

    feat: [PAGOPA-3454] Integration tests add new methods receipt datastore client for helpdesk

--- integration-test/src/step_definitions/support/receipts_datastore_client.js
+++ integration-test/src/step_definitions/support/receipts_datastore_client.js
@@ -1,28 +1,28 @@
 const { CosmosClient } = require("@azure/cosmos");
-const { createReceipt, createCartReceipt } = require("./common");
+const { createReceipt, createCartReceipt, createReceiptError, createReceiptMessage, createReceiptCartError } = require("./common");
 
-const cosmos_db_conn_string     = process.env.RECEIPTS_COSMOS_CONN_STRING || "";
-const databaseId                = process.env.RECEIPT_COSMOS_DB_NAME;
-const receiptContainerId        = process.env.RECEIPT_COSMOS_DB_CONTAINER_NAME;
-const receiptCartContainerId    = process.env.RECEIPT_CART_COSMOS_DB_CONTAINER_NAME;
+const cosmos_db_conn_string = process.env.RECEIPTS_COSMOS_CONN_STRING || "";
+const databaseId = process.env.RECEIPT_COSMOS_DB_NAME;
+const receiptContainerId = process.env.RECEIPT_COSMOS_DB_CONTAINER_NAME;
+const receiptErrorContainerId = process.env.RECEIPT_ERROR_COSMOS_DB_CONTAINER_NAME;
+const receiptMessageContainerId = process.env.RECEIPT_MESSAGE_COSMOS_DB_CONTAINER_NAME;
+const receiptCartContainerId = process.env.RECEIPT_CART_COSMOS_DB_CONTAINER_NAME;
+const receiptCartErrorContainerId = process.env.RECEIPT_CART_ERROR_COSMOS_DB_CONTAINER_NAME;
+const receiptCartMessageContainerId = process.env.RECEIPT_CART_MESSAGE_COSMOS_DB_CONTAINER_NAME;
 
 const client = new CosmosClient(cosmos_db_conn_string);
 const receiptContainer = client.database(databaseId).container(receiptContainerId);
+const receiptErrorContainer = client.database(databaseId).container(receiptErrorContainerId);
+const receiptMessageContainer = client.database(databaseId).container(receiptMessageContainerId);
 const receiptCartContainer = client.database(databaseId).container(receiptCartContainerId);
+const receiptCartErrorContainer = client.database(databaseId).container(receiptCartErrorContainerId);
+const receiptCartMessageContainer = client.database(databaseId).container(receiptCartMessageContainerId);
 
+// RECEIPT
 async function createDocumentInReceiptsDatastore(id, fiscalCode, pdfName) {
-    let event = createReceipt(id, fiscalCode, pdfName);
+    let receipt = createReceipt(id, fiscalCode, pdfName);
     try {
-        return await receiptContainer.items.create(event);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function createDocumentInReceiptsCartDatastore(id, payerFiscalCode, payerBizEventId, debtorFiscalCode, debtorBizEventId, pdfName) {
-    let event = createCartReceipt(id, payerFiscalCode, payerBizEventId, debtorFiscalCode, debtorBizEventId, pdfName);
-    try {
-        return await receiptCartContainer.items.create(event);
+        return await receiptContainer.items.create(receipt);
     } catch (err) {
         console.log(err);
     }
@@ -38,6 +38,56 @@ async function deleteDocumentFromReceiptsDatastore(id, partitionKey) {
     }
 }
 
+//RECEIPT ERROR
+async function createDocumentInReceiptErrorDatastore(id, status) {
+    let receipt = createReceiptError(id, status);
+    try {
+        return await receiptErrorContainer.items.create(receipt);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteDocumentFromReceiptsErrorDatastore(id, partitionKey) {
+    try {
+        return await receiptErrorContainer.item(id, partitionKey).delete();
+    } catch (error) {
+        if (error.code !== 404) {
+            console.log(error)
+        }
+    }
+}
+
+//RECEIPT MESSAGE
+async function createDocumentInReceiptIoMessageDatastore(eventId, messageId) {
+    let message = createReceiptMessage(eventId, messageId);
+    try {
+        return await receiptMessageContainer.items.create(message);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteDocumentInReceiptIoMessageDatastore(id) {
+    try {
+        return await receiptMessageContainer.item(id, id).delete();
+    } catch (error) {
+        if (error.code !== 404) {
+            console.log(error)
+        }
+    }
+}
+
+// CART RECEIPT
+async function createDocumentInReceiptsCartDatastore(id, payerFiscalCode, payerBizEventId, debtorFiscalCode, debtorBizEventId, pdfName) {
+    let receipt = createCartReceipt(id, payerFiscalCode, payerBizEventId, debtorFiscalCode, debtorBizEventId, pdfName);
+    try {
+        return await receiptCartContainer.items.create(receipt);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 async function deleteDocumentFromReceiptsCartDatastore(id, partitionKey) {
     try {
         return await receiptCartContainer.item(id, partitionKey).delete();
@@ -48,7 +98,94 @@ async function deleteDocumentFromReceiptsCartDatastore(id, partitionKey) {
     }
 }
 
+// CART RECEIPT ERROR
+async function createDocumentInReceiptsCartErrorDatastore(id) {
+    let receipt = createReceiptCartError(id);
+    try {
+        return await receiptCartErrorContainer.items.create(receipt);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteDocumentFromReceiptsCartErrorDatastore(id, partitionKey) {
+    try {
+        return await receiptCartErrorContainer.item(id, partitionKey).delete();
+    } catch (error) {
+        if (error.code !== 404) {
+            console.log(error)
+        }
+    }
+}
+
+//CART RECEIPT MESSAGE
+async function createDocumentInReceiptCartIoMessageDatastore(eventId, messageId) {
+    let message = createReceiptMessage(eventId, messageId);
+    try {
+        return await receiptCartMessageContainer.items.create(message);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteDocumentInReceiptCartIoMessageDatastore(id) {
+    try {
+        return await receiptCartMessageContainer.item(id, id).delete();
+    } catch (error) {
+        if (error.code !== 404) {
+            console.log(error)
+        }
+    }
+}
+
+// CLEANUP METHODS
+async function getDocumentFromReceiptsDatastoreByEventId(id) {
+    return await receiptContainer.items
+        .query({
+            query: "SELECT * from c WHERE c.eventId=@eventId",
+            parameters: [{ name: "@eventId", value: id }]
+        })
+        .fetchNext();
+}
+async function deleteMultipleDocumentsFromReceiptsDatastoreByEventId(eventId) {
+    let documents = await getDocumentFromReceiptsDatastoreByEventId(eventId);
+
+    documents?.resources?.forEach(el => {
+        deleteDocumentFromReceiptsDatastore(el.id);
+    })
+}
+
+async function getDocumentFromReceiptsErrorDatastoreByBizEventId(id) {
+    return await receiptErrorContainer.items
+        .query({
+            query: "SELECT * from c WHERE c.bizEventId=@bizEventId",
+            parameters: [{ name: "@bizEventId", value: id }]
+        })
+        .fetchNext();
+}
+async function deleteMultipleDocumentFromReceiptErrorDatastoreByEventId(id) {
+    let documents = await getDocumentFromReceiptsErrorDatastoreByBizEventId(id);
+
+    documents?.resources?.forEach(el => {
+        deleteDocumentFromReceiptsErrorDatastore(el.id);
+    })
+}
+
 module.exports = {
-    createDocumentInReceiptsDatastore, deleteDocumentFromReceiptsDatastore,
-    createDocumentInReceiptsCartDatastore, deleteDocumentFromReceiptsCartDatastore
+    createDocumentInReceiptsDatastore,
+    deleteDocumentFromReceiptsDatastore,
+    createDocumentInReceiptErrorDatastore,
+    deleteDocumentFromReceiptsErrorDatastore,
+    createDocumentInReceiptIoMessageDatastore,
+    deleteDocumentInReceiptIoMessageDatastore,
+
+    createDocumentInReceiptsCartDatastore,
+    deleteDocumentFromReceiptsCartDatastore,
+    createDocumentInReceiptsCartErrorDatastore,
+    deleteDocumentFromReceiptsCartErrorDatastore,
+    createDocumentInReceiptCartIoMessageDatastore,
+    deleteDocumentInReceiptCartIoMessageDatastore,
+
+    deleteMultipleDocumentsFromReceiptsDatastoreByEventId,
+    deleteMultipleDocumentFromReceiptErrorDatastoreByEventId
 }
