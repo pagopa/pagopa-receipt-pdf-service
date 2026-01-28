9bca29241d4ec19c39ea655bda1e19a73ba88107
svariant <samuele.varianti@nttdata.com>
2026-01-28 17:42:50 +0100

    chore: [PIDM-1421] Clean imports

--- src/test/java/it/gov/pagopa/receipt/pdf/service/utils/OpenApiGenerationPDFTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/service/utils/OpenApiGenerationPDFTest.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.receipt.pdf.service.utils;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
-import it.gov.pagopa.receipt.pdf.service.utils.profiles.HelpdeskProfile;
 import it.gov.pagopa.receipt.pdf.service.utils.profiles.PdfProfile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
